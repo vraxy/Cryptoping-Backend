c693f1e2722a4201270404f285f17910226fcf3b
Sumit Lahiri <shekharhiran@hotmail.com>
2019-03-16 19:21:33 +0530

    Commit

--- routes/bittrex.js
+++ routes/bittrex.js
@@ -2,6 +2,12 @@
 var router = express.Router();
 var request = require('request');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 router.get('/bittrex', function (req, res) {
     request('https://api.bittrex.com/api/v1.1/public/getmarketsummaries', function (error, response, body) {
         if (!error && response.statusCode === 200) {
@@ -13,5 +19,18 @@ router.get('/bittrex', function (req, res) {
         }
     })
 });
+
+router.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
 
 module.exports = router;
